efd81c9526040b389abb769caf88bcd48ee4fbb4
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2024-01-13 05:45:57 +0000

    Update oceanScan.js

--- src/pages/ocean/oceanScan.js
+++ src/pages/ocean/oceanScan.js
@@ -102,9 +102,21 @@ const oceanScan = () => {
           <div className="flex flex-col min-h-[60vh] items-center justify-center w-full">
             <span className="text-md font-bold">RESULT</span>
             <span className="text-md font-semibold mt-2">
-             {data}
+              <label
+                for="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your result
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..."
+              >
+                {data}
+              </textarea>
             </span>
-           
           </div>
         </div>
 
